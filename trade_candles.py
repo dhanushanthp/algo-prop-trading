c331edac8fe8647270b327041db04cc20f3af5f8
Dhanushanth <dhanushanthp@gmail.com>
2023-11-16 10:06:18 +1100

    adding session times.

--- trade_candles.py
+++ trade_candles.py
@@ -377,32 +377,31 @@ class TradeCandle():
         positions = mt.positions_get()
 
         for obj in positions: 
-            if obj.symbol == self.symbol:
-                if obj.type == 1: 
-                    order_type = mt.ORDER_TYPE_BUY
-                    price = mt.symbol_info_tick(obj.symbol).bid
-                else:
-                    order_type = mt.ORDER_TYPE_SELL
-                    price = mt.symbol_info_tick(obj.symbol).ask
-                
-                close_request = {
-                    "action": mt.TRADE_ACTION_DEAL,
-                    "symbol": obj.symbol,
-                    "volume": obj.volume,
-                    "type": order_type,
-                    "position": obj.ticket,
-                    "price": price,
-                    "deviation": 20,
-                    "magic": 234000,
-                    "comment": 'Close trade',
-                    "type_time": mt.ORDER_TIME_GTC,
-                    "type_filling": mt.ORDER_FILLING_IOC, # also tried with ORDER_FILLING_RETURN
-                }
-                
-                result = mt.order_send(close_request) # send order to close a position
-                
-                if result.retcode != mt.TRADE_RETCODE_DONE:
-                    print("Close Order "+obj.symbol+" failed!!...comment Code: "+str(result.comment))
+            if obj.type == 1: 
+                order_type = mt.ORDER_TYPE_BUY
+                price = mt.symbol_info_tick(obj.symbol).bid
+            else:
+                order_type = mt.ORDER_TYPE_SELL
+                price = mt.symbol_info_tick(obj.symbol).ask
+            
+            close_request = {
+                "action": mt.TRADE_ACTION_DEAL,
+                "symbol": obj.symbol,
+                "volume": obj.volume,
+                "type": order_type,
+                "position": obj.ticket,
+                "price": price,
+                "deviation": 20,
+                "magic": 234000,
+                "comment": 'Close trade',
+                "type_time": mt.ORDER_TIME_GTC,
+                "type_filling": mt.ORDER_FILLING_IOC, # also tried with ORDER_FILLING_RETURN
+            }
+            
+            result = mt.order_send(close_request) # send order to close a position
+            
+            if result.retcode != mt.TRADE_RETCODE_DONE:
+                print("Close Order "+obj.symbol+" failed!!...comment Code: "+str(result.comment))
 
     def enable_symbol(self):
         if not mt.symbol_select(self.symbol,True):
--- util.py
+++ util.py
@@ -39,10 +39,11 @@ def get_market_status():
     market_open = False
     market_about_to_close= False
     day, hour, minute = get_gmt_time()
+    print(day, hour, minute)
 
     if day not in ["Saturday","Sunday"]:
         # Once market open become disabled, No new trades
-        if (hour >= 0 and minute > 30) or (hour < 22):
+        if (hour >= 0 and minute >= 30) or (hour < 23):
             market_open = True
     
     if (day in ["Saturday","Sunday"]) or (hour >= 23 and minute >= 30):
