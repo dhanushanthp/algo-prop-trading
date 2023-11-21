03cdfd1c9ecd8e04d169afa2608af97e2044b0bc
Dhanushanth <dhanushanthp@gmail.com>
2023-11-22 06:05:09 +1100

    F

--- profit_taker.py
+++ profit_taker.py
@@ -1,11 +1,13 @@
 import indicators as ind
 import mng_pos as mp
+from datetime import datetime
 
 while True:
     mp.breakeven_1R_positions()
     account_size, equity, _, total_active_profit = ind.get_account_details()
     
-    if total_active_profit > 100:
+    if total_active_profit > 50:
+        print(f"Profit taking hit @ {datetime.now().strftime('%H:%M:%S')}")
         mp.close_all_positions()
     
     # if equity < account_size-100:
