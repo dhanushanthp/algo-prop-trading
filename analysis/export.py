fc188e943e9c6ea71f052f898bd59e0db43fbf06
Dhanushanth <dhanushanthp@gmail.com>
2023-12-01 06:34:46 +1100

    F

--- analysis/export.py
+++ analysis/export.py
@@ -8,7 +8,7 @@ mt5.initialize()
 
 def get_today_profit(file_name):
     tm_zone = pytz.timezone('Etc/GMT-2')
-    start_time = datetime.combine(datetime(2023, 11, 20, tzinfo=tm_zone) , time())  
+    start_time = datetime.combine(datetime(2023, 11, 27, tzinfo=tm_zone) , time())  
     end_time = datetime.now(tm_zone) + timedelta(hours=4)
     data = mt5.history_deals_get(start_time, end_time)
     df=pd.DataFrame(list(data),columns=data[0]._asdict().keys())
