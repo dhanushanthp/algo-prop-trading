d67bcc9b7ccef52d064114c65a98a69f430dac1a
Dhanushanth <dhanushanthp@gmail.com>
2023-12-09 07:48:36 +1100

    adding logging

---
+++ file_utils.py
@@ -0,0 +1,29 @@
+class FileUtils:
+    def __init__(self, directory) -> None:
+        self.directory = directory
+    
+    def log_writer(self, trade_time, write_line, write_mode="a", sep="\t"):
+        """
+        Write the logs
+
+        Args:
+            input (str): Input String
+            trade_time (str): Keep the time format as follows : "%Y%m%d %H:%M:%S"
+            write_mode (str, optional): _description_. Defaults to "a".
+        """
+        date_split = trade_time.split(" ")
+        date = date_split[0]
+        hour = int(date_split[-1].split(":")[0])
+        
+        # When the input is list, convert to string and joing by commma
+        if isinstance(write_line, list):
+            write_line = ','.join([str(i) for i in write_line])
+        
+        with open("session.logs", write_mode) as f:
+            # Prepend the time
+            f.write(f"{trade_time}{sep}{write_line}\n")
+            f.close()
+            
+if __name__ == "__main__":
+    obj = FileUtils("/")
+    obj.log_writer("testing,adfad")
---
+++ slack_msg.py
@@ -0,0 +1,24 @@
+import os
+from slack import WebClient
+import logging
+from slack.errors import SlackApiError
+log = logging.getLogger("SLACK")
+
+
+class Slack:
+    def __init__(self) -> None:
+        self.client = WebClient(
+            token="xoxb-4628404917445-4631574171810-nrebpFdu1u7M3mpmy0H5y0Pq")
+        self.SLACK_CHANNEL = 'general'
+
+    def send_msg(self, msg):
+        if True:
+            try:
+                self.client.chat_postMessage(channel='#general', text=msg)
+            except SlackApiError as e:
+                log.info(msg)
+
+
+if __name__ == "__main__":
+    ref = Slack()
+    ref.send_msg('testing')
