aad5f65fd647ea41b7a64a8f6d71cf261320e837
Dhanushanth <dhanushanthp@gmail.com>
2023-12-26 15:16:45 +1100

    refactor

--- trade_candles_r_s_combined.py
+++ trade_candles_r_s_combined.py
@@ -160,8 +160,8 @@ class AlgoTrader():
         
         while True:
             print(f"\n-------  {self.strategy.upper()} @ {datetime.now().strftime('%H:%M:%S')}------------------")
-            print(f"{'Current Risk'.ljust(20)}: ${round(self.risk_manager.initial_risk, 2)}, Account Max Loss: ${self.risk_manager.account_max_loss}")
-            print(f"{'Max Loss'.ljust(20)}: ${round(self.risk_manager.get_max_loss())}, trail ${self.risk_manager.account_max_loss}")
+            print(f"{'Current Risk'.ljust(20)}: ${round(self.risk_manager.initial_risk, 2)}")
+            print(f"{'Max Loss'.ljust(20)}: ${round(self.risk_manager.get_max_loss())}, Trail/Acc Max Loss ${self.risk_manager.account_max_loss}")
             print(f"{'Trail Update at'.ljust(20)}: ${round(self.risk_manager.get_max_loss() + self.risk_manager.account_max_loss)}")
             
             is_market_open, is_market_close = util.get_market_status()
