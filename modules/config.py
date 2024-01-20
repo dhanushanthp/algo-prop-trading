5e3f1afb32d451471b19643add2df5008d91a08e
Dhanushanth <dhanushanthp@gmail.com>
2024-01-20 23:41:57 +1100

    adding customized risk management

--- modules/config.py
+++ modules/config.py
@@ -1,3 +1,13 @@
-account_risk_percentage = 0.5 # Value in %
+import modules.currency_pairs as curr
+
+if curr.company == "FTMO S.R.O.":
+    account_risk_percentage = 1.0 # Value in %
+elif curr.company == "AXSE Brokerage Ltd.":
+    account_risk_percentage = 0.5 # Value in %
+elif curr.company == "Black Bull Group Limited":
+    account_risk_percentage = 0.5 # Value in %
+else:
+    account_risk_percentage = 1.0 # Value in %
+
 position_split_of_account_risk = 4 # Number of positions can risk as per total account risk
 server_timezone=2
