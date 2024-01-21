e92c1d32b95b95d53733d471ce53a846a6b9e2d7
Dhanushanth <dhanushanthp@gmail.com>
2024-01-22 10:35:50 +1100

    account based risk levels

--- modules/config.py
+++ modules/config.py
@@ -1,11 +1,11 @@
 import modules.currency_pairs as curr
 
 if curr.company == "FTMO S.R.O.":
-    account_risk_percentage = 1.0 # Value in %
+    account_risk_percentage = 0.5 # Value in %
 elif curr.company == "AXSE Brokerage Ltd.":
-    account_risk_percentage = 0.5 # Value in %
+    account_risk_percentage = 0.25 # Value in %
 elif curr.company == "Black Bull Group Limited":
-    account_risk_percentage = 0.5 # Value in %
+    account_risk_percentage = 0.25 # Value in %
 else:
     account_risk_percentage = 1.0 # Value in %
 
