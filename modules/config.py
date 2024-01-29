bbad7444fc9966c906bf60b0a4387ecc1326c718
Dhanushanth <dhanushanthp@gmail.com>
2024-01-29 23:20:11 +1100

    F

--- modules/config.py
+++ modules/config.py
@@ -4,9 +4,9 @@ import socket
 if curr.company == "FTMO S.R.O.":
     account_risk_percentage = 1.0 # Value in %
 elif curr.company == "AXSE Brokerage Ltd.":
-    account_risk_percentage = 0.20 # Value in %
+    account_risk_percentage = 1.0 # Value in %
 elif curr.company == "Black Bull Group Limited":
-    account_risk_percentage = 0.20 # Value in %
+    account_risk_percentage = 1.0 # Value in %
 else:
     account_risk_percentage = 1.0 # Value in %
 
