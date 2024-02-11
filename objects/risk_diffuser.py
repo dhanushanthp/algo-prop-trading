5c3d27d6ac17f83f3c4d93ccb54afe0d68184f9e
Dhanushanth <dhanushanthp@gmail.com>
2024-02-11 13:50:36 +1100

    F

--- objects/risk_diffuser.py
+++
@@ -1,6 +0,0 @@
-class RiskDiffuser:
-    def __init__(self, direction ,symbol, stop_price, position_risk):
-        self.direction = direction
-        self.symbol = symbol
-        self.stop_price = stop_price
-        self.position_risk = position_risk
