d77b3293f6b886776072a0d9a68b4867436cb8f9
Dhanushanth <dhanushanthp@gmail.com>
2024-02-23 08:52:40 +1100

    F

--- snipper_ranger.bat
+++ snipper_ranger.bat
@@ -1,10 +1,10 @@
 python sniper_ranger.py ^
-    --timeframe 15 ^
+    --strategy %1 ^
+    --timeframe %2 ^
     --account_risk 2.0 ^
     --each_position_risk 0.5 ^
     --target_ratio 1.0 ^
     --early_profit no ^
     --trace_exit no ^
     --persist_data yes ^
-    --partial_rr 1.2 ^
-    --strategy %1
+    --partial_rr 1.2
