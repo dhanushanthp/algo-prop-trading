4a0828b67a6a5d648b03d5f7e0e682489b8b2021
Dhanushanth <dhanushanthp@gmail.com>
2024-03-06 00:15:57 +1100

    F

--- snipper_ranger.bat
+++ snipper_ranger.bat
@@ -1,12 +1,16 @@
 @REM test: sniper_ranger.bat break no yes
 @REM real: sniper_ranger.bat break yes no
 
+set account_risk=1.0
+set timeframe=60
+set target_ratio=2.0
+
 python sniper_ranger.py ^
     --strategy %1 ^
-    --timeframe 60 ^
-    --account_risk 1.0 ^
+    --timeframe %timeframe% ^
+    --account_risk %account_risk% ^
     --each_position_risk 0.5 ^
-    --target_ratio 2.0 ^
+    --target_ratio %target_ratio% ^
     --early_profit yes ^
     --early_rr 1.1 ^
     --trace_exit %2 ^
