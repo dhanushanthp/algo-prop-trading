2f41669160a786de0fb75b2f374eec9a7ba51db7
Dhanushanth <dhanushanthp@gmail.com>
2024-03-06 09:04:31 +1100

    F

--- sniper_ranger.py
+++ sniper_ranger.py
@@ -75,7 +75,8 @@ class SniperReloaded():
         selected_symbols = curr.get_ordered_symbols()
         
         while True:
-            print(f"\n------- {config.local_ip.replace('_', '.')} @ {util.get_current_time().strftime('%H:%M:%S')} in {self.trading_timeframe} TF {self.strategy.upper()}, Adddtional: {self.addtional_levels}, Trace Exit:{self.trace_exit}, Early Profit:{self.early_profit} ({self.early_rr} RR) -----------")
+            # {config.local_ip.replace('_', '.')} @ {util.get_current_time().strftime('%H:%M:%S')} in 
+            print(f"\n------- {self.trading_timeframe} TF {self.strategy.upper()}, Adddtional: {self.addtional_levels}, Trace Exit:{self.trace_exit}, Early Profit:{self.early_profit} ({self.early_rr} RR) -----------")
             is_market_open, is_market_close = util.get_market_status()
             equity = self.account.get_equity()
             rr = (equity - self.fixed_initial_account_size)/self.risk_manager.risk_of_an_account
