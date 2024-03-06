932ebfc741834403c4e4d8ab8015f0c0340d846d
Dhanushanth <dhanushanthp@gmail.com>
2024-03-07 10:55:29 +1100

    F

--- clients/rest.py
+++ clients/rest.py
@@ -1,13 +1,22 @@
 import requests
 import json
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class Rest:
     def __init__(self) -> None:
         self.base_url = "https://dxtrade.ftmo.com/dxsca-web/"
 
-    def post_request(self, url, payload):
+    def post_request(self, url, payload, headers=None):
         request_url = self.base_url + url
-        response = requests.post(request_url, json=payload, verify=False)
+        
+        if not headers:
+            headers = {}
+        
+        if payload:
+            response = requests.post(request_url, json=payload, verify=False, headers=headers)
+        else:
+            response = requests.post(request_url, verify=False, headers=headers)
 
         if response.status_code == 404:
             return response.status_code, response.content
@@ -24,3 +33,27 @@ class Rest:
             return response.status_code, response.content
 
         return response.status_code, order_content
+    
+    def get_request(self, url, headers):
+        """
+        Base request to the Client Portal
+        """
+        request_url = self.base_url + url
+        
+        if not headers:
+            headers = {'Content-Type': 'application/json'}
+
+        response = requests.get(request_url, verify=False, headers=headers)
+        return json.loads(response.content)
+
+    def delete_request(self, url, headers):
+        """
+        Base request to the Client Portal
+        """
+        request_url = self.base_url + url
+        
+        if not headers:
+            headers = {'Content-Type': 'application/json'}
+
+        response = requests.delete(request_url, verify=False, headers=headers)
+        return json.loads(response.content)
