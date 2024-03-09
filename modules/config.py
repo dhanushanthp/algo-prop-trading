975b1a5d2493e415b39073f887d258644ff346ac
Dhanushanth <dhanushanthp@gmail.com>
2024-03-10 09:41:01 +1100

    F

--- modules/config.py
+++ modules/config.py
@@ -1,10 +1,4 @@
 import socket
-
-account_risk_percentage = 1.0 # Value in %
-num_positions_at_risk = 5 # Number of positions can risk as per total account risk
-
 buffer_ratio=0.25
-
 server_timezone=2
 local_ip = socket.gethostbyname(socket.gethostname()).replace(".", "_")
-risk_of_a_position=account_risk_percentage/num_positions_at_risk
