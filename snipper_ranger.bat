e8b4b643c4665877a8c24b1f1d05ad11e9238baa
Dhanushanth <dhanushanthp@gmail.com>
2024-03-13 21:57:36 +1100

    F

--- snipper_ranger.bat
+++ snipper_ranger.bat
@@ -1,10 +1,10 @@
-@REM sniper_ranger.bat 1% timeframe
+@REM sniper_ranger.bat 1% timeframe break
 
 set account_risk=%1
 set target_ratio=2.0
 
 python sniper_ranger.py ^
-    --strategy break ^
+    --strategy %3 ^
     --timeframe %2 ^
     --account_risk %account_risk% ^
     --each_position_risk 0.5 ^
