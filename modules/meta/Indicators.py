4dc46a7ed931d3b0a5b13eb3b619a2868d049dca
Dhanushanth <dhanushanthp@gmail.com>
2024-03-17 20:18:20 +1100

    F

--- modules/meta/Indicators.py
+++ modules/meta/Indicators.py
@@ -10,11 +10,13 @@ from modules.common.Signal import Signal
 from typing import Tuple, List, Dict
 from modules.common import logme
 from modules.meta.wrapper import Wrapper
+from modules.meta.Prices import Prices
 from modules.common.Directions import Directions
 
 class Indicators:
     def __init__(self) -> None:
         self.wrapper = Wrapper()
+        self.prices = Prices()
 
     def get_atr(self, symbol:str, timeframe:int) -> float:
         """
@@ -110,6 +112,91 @@ class Indicators:
             pre_market_low = Signal(reference="LOD", level=min(previous_bars["low"]), break_bar_index=previous_bars["low"].idxmin())
             return pre_market_high, pre_market_low
 
+
+    def find_pivots(self, symbol, timeframe):
+        """
+        Identifies potential support and resistance levels based on recent price behavior.
+
+        Parameters:
+        - symbol (str): The trading symbol for the financial instrument.
+        - timeframe (str): The timeframe for historical price data (e.g., 'M1', 'H1', 'D1').
+
+        Returns:
+        dict: A dictionary containing identified support and resistance levels.
+            Example:
+            {
+                'support': [support_level1, support_level2, ...],
+                'resistance': [resistance_level1, resistance_level2, ...]
+            }
+
+        The function analyzes recent price behavior by examining the highs and lows of the past three candles.
+        It identifies potential resistance levels when the difference between the high of the middle candle
+        and the highs of the adjacent candles is greater than 3 times the spread. Similarly, it identifies
+        potential support levels when the difference between the low of the middle candle and the lows of
+        the adjacent candles is greater than 3 times the spread.
+
+        The function further filters out levels that intersect with previous candles to provide clean support
+        and resistance levels.
+
+        Note: The function relies on a helper function, `match_timeframe`, and assumes the existence of
+        another helper function, `is_number_between`.
+
+        :param symbol: Trading symbol for the financial instrument.
+        :param timeframe: Timeframe for recent price data.
+        :return: A dictionary with 'support' and 'resistance' levels.
+        """
+        
+        # If does the mid values intersect with previous 5 bars
+        # get past 5 candles and start from prevous second candle
+        past_candles = self.wrapper.get_candles_by_index(symbol=symbol, timeframe=timeframe, candle_index_start=2, candle_index_end=30, return_type="list")
+        past_candles.reverse()
+
+        resistance_levels = {}
+        suport_levels = {}
+
+        for i in range(len(past_candles) - 2):
+            end_candle = past_candles[i]
+            middle_candle = past_candles[i+1]
+            start_candle = past_candles[i+2]
+            spread = 3 * self.prices.get_spread(symbol=symbol)
+
+            if ((middle_candle['high'] - end_candle["high"]) > spread) and ((middle_candle['high'] - start_candle["high"]) > spread):
+                resistance_levels[i] =  middle_candle["high"]
+            
+            if ((end_candle["low"] - middle_candle['low']) > spread) and ((start_candle["low"] - middle_candle['low']) > spread):
+                suport_levels[i] = middle_candle["low"]
+
+
+        # Filter resistance levels, The levels should not intersect with any previous candle
+        breaked_resistances = []
+        breaked_supprots = []
+
+        for res in resistance_levels.keys():
+            res_level = resistance_levels[res]
+            upcoming_candles = past_candles[:res]
+            for candle in upcoming_candles:
+                if self.is_number_between(res_level, candle["low"], candle["high"]):
+                    breaked_resistances.append(res_level)
+                    break
+
+        for supp in suport_levels.keys():
+            supp_level = suport_levels[supp]
+            upcoming_candles = past_candles[:supp]
+            for candle in upcoming_candles:
+                if self.is_number_between(supp_level, candle["low"], candle["high"]):
+                    breaked_supprots.append(supp_level)
+                    break
+        
+        clean_resistance = [i for i in resistance_levels.values() if i not in breaked_resistances]
+        clean_support = [i for i in suport_levels.values() if i not in breaked_supprots]
+
+        return {"support": clean_support, "resistance": clean_resistance}
+        
+    def is_number_between(self, number, lower_limit, upper_limit):
+        if lower_limit > upper_limit:
+            return lower_limit > number > upper_limit
+        else:
+            return lower_limit < number < upper_limit
 
     def get_king_of_levels(self, symbol, timeframe) -> Dict[str, List[Signal]]:
         highs = []
