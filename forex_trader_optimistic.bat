4b15c4b938b1a909a9128ca255d4dbe8a00f351e
Dhanushanth <dhanushanthp@gmail.com>
2024-04-02 09:38:27 +1100

    F

--- forex_trader_optimistic.bat
+++ forex_trader_optimistic.bat
@@ -1,7 +1,7 @@
 @REM sniper_ranger.bat 1% timeframe break
 
 set account_risk=1
-set target_ratio=8.0
+set target_ratio=10.0
 set security=FOREX
 
 python smart_trader.py ^
@@ -9,5 +9,5 @@ python smart_trader.py ^
     --security %security% ^
     --timeframe 60 ^
     --account_risk %account_risk% ^
-    --each_position_risk 0.5 ^
+    --each_position_risk 0.25 ^
     --target_ratio %target_ratio%
