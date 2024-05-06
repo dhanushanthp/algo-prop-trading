841164cdff4a878dae76c4573c36d488a13e87de
Dhanushanth <dhanushanthp@gmail.com>
2024-05-06 22:49:42 +1000

    F

--- smart_trader.py
+++ smart_trader.py
@@ -279,6 +279,30 @@ class SmartTrader():
                                     if  candle_gap > 2:
                                         if self.trade(direction=Directions.SHORT, symbol=symbol, reference=low_of_day.reference, break_level=candle_gap):
                                             break # Break the support loop
+
+                            case "DAILY_HL_DOUBLE_HIT":
+                                """
+                                Levels such as High of the Day, Low of the day will be checked with previous bar close
+                                """
+                                high_of_day, low_of_day = self.indicators.get_current_day_levels(symbol=symbol, 
+                                                                                  timeframe=self.trading_timeframe, 
+                                                                                  start_reference_bar=2)
+                                
+                                previous_candle = self.wrapper.get_todays_candles(symbol=symbol,
+                                                                                  timeframe=self.trading_timeframe,
+                                                                                  start_candle=1).iloc[-1]
+
+                                if (previous_candle["low"] < high_of_day.level and previous_candle["close"] > high_of_day.level):
+                                    candle_gap = previous_candle["index"] - high_of_day.break_bar_index
+                                    if  candle_gap > 2:
+                                        if self.trade(direction=Directions.LONG, symbol=symbol, reference=high_of_day.reference, break_level=candle_gap):
+                                            break # Break the resistance loop
+                            
+                                if (previous_candle["high"] > low_of_day.level and previous_candle["close"] < low_of_day.level):   
+                                    candle_gap = previous_candle["index"] - low_of_day.break_bar_index
+                                    if  candle_gap > 2:
+                                        if self.trade(direction=Directions.SHORT, symbol=symbol, reference=low_of_day.reference, break_level=candle_gap):
+                                            break # Break the support loop
                             
                             case "WEEKLY_HL":
                                 """
