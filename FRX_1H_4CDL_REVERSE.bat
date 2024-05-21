20beb87965805a69b3db2e48027ac405075a59dd
Dhanushanth <dhanushanthp@gmail.com>
2024-05-21 14:24:15 +1000

    F

--- FRX_1H_4CDL_REVERSE.bat
+++ FRX_1H_4CDL_REVERSE.bat
@@ -15,7 +15,7 @@ python smart_trader.py ^
     --strategy %strategy% ^
     --enable_trail_stop %enable_trail_stop% ^
     --enable_breakeven %enable_breakeven% ^
-    --enable_neutralizer %enable_neutralizer% ^ 
+    --enable_neutralizer %enable_neutralizer% ^
     --trades_per_day %trades_per_day% ^
     --num_prev_cdl_for_stop 2 ^
     --each_position_risk %each_position_risk% ^
