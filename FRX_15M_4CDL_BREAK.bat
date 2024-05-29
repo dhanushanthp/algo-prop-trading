dbe7f776b4deca19e70fbd2312a421f11674c8a7
Dhanushanth <dhanushanthp@gmail.com>
2024-05-30 07:50:25 +1000

    F

---
+++ FRX_15M_4CDL_BREAK.bat
@@ -0,0 +1,27 @@
+set timeframe=15
+set account_risk=1.0
+set each_position_risk=0.1
+set trades_per_day=15
+set target_ratio=4.0
+set security=FOREX
+set systems=4CDL_REV_EXT
+set strategy=BREAK
+set enable_trail_stop=no
+set enable_breakeven=no
+set enable_neutralizer=yes
+set start_hour=4
+
+python smart_trader.py ^
+    --strategy %strategy% ^
+    --enable_trail_stop %enable_trail_stop% ^
+    --enable_breakeven %enable_breakeven% ^
+    --enable_neutralizer %enable_neutralizer% ^
+    --trades_per_day %trades_per_day% ^
+    --num_prev_cdl_for_stop 2 ^
+    --each_position_risk %each_position_risk% ^
+    --systems %systems% ^
+    --security %security% ^
+    --timeframe %timeframe% ^
+    --account_risk %account_risk% ^
+    --start_hour %start_hour% ^
+    --target_ratio %target_ratio%
