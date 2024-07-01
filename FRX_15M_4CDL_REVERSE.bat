489487033f1f1778e77b29b4eb0a292953aefbc4
Dhanushanth <dhanushanthp@gmail.com>
2024-07-01 22:36:26 +1000

    F

--- FRX_15M_4CDL_REVERSE.bat
+++ FRX_15M_4CDL_REVERSE.bat
@@ -9,9 +9,9 @@ set timeframe=15
 set trades_per_day=100
 
 REM Risk Management
-set account_risk=1.0
-set each_position_risk=0.1
-set target_ratio=10.0
+set account_risk=0.5
+set each_position_risk=0.05
+set target_ratio=5.0
 set enable_dynamic_position_risk=no
 set max_loss_exit=yes
 set max_target_exit=yes
