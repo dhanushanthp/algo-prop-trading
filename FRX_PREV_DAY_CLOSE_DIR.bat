33a777e0d7135c4d4e620ea1194c7083200bc658
Dhanushanth <dhanushanthp@gmail.com>
2024-07-09 19:03:16 +1000

    F

--- FRX_PREV_DAY_CLOSE_DIR.bat
+++ FRX_PREV_DAY_CLOSE_DIR.bat
@@ -10,7 +10,7 @@ set trades_per_day=100
 
 REM Risk Management
 set account_risk=0.5
-set each_position_risk=0.05
+set each_position_risk=0.1
 set target_ratio=10.0
 set enable_dynamic_position_risk=no
 set max_loss_exit=yes
