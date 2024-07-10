79d26af1b1485a43afe115abff628e7d2193105a
Dhanushanth <dhanushanthp@gmail.com>
2024-07-10 20:35:43 +1000

    F

--- FRX_PREV_DAY_CLOSE_DIR.bat
+++ FRX_PREV_DAY_CLOSE_DIR.bat
@@ -9,12 +9,12 @@ set timeframe=15
 set trades_per_day=100
 
 REM Risk Management
-set account_risk=0.5
-set each_position_risk=0.1
+set account_risk=0.50
+set each_position_risk=0.05
 set target_ratio=10.0
 set enable_dynamic_position_risk=no
 set max_loss_exit=yes
-set max_target_exit=no
+set max_target_exit=yes
 
 REM Strategy and System
 set strategy=BREAK
