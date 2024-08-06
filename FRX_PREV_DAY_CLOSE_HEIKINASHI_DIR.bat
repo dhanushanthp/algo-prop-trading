60f97a37df5cdfe2c81e4a0d590db08b5c407610
Dhanushanth <dhanushanthp@gmail.com>
2024-08-06 10:57:26 +1000

    F

--- FRX_PREV_DAY_CLOSE_HEIKINASHI_DIR.bat
+++ FRX_PREV_DAY_CLOSE_HEIKINASHI_DIR.bat
@@ -13,7 +13,7 @@ set account_risk=0.50
 set each_position_risk=0.05
 set target_ratio=10.0
 set account_target_ratio=2.1
-set enable_dynamic_position_risk=yes
+set enable_dynamic_position_risk=no
 set max_loss_exit=yes
 set max_target_exit=no
 
