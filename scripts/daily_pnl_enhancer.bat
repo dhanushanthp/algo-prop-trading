f08bd98b72aece318db1e4dca469565c7c7808b0
Dhanushanth <dhanushanthp@gmail.com>
2024-12-09 19:41:48 +1100

    F

---
+++ scripts/daily_pnl_enhancer.bat
@@ -0,0 +1,2 @@
+call "C:\Users\%USERNAME%\OneDrive\Financial Freedom\Phoenix\scripts\set_path.bat"
+python "C:\Users\%USERNAME%\OneDrive\Financial Freedom\Phoenix\modules\analysis\target_exit_analysier.py"
