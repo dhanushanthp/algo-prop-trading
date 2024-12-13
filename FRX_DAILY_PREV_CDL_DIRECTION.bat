130cd2adb812ee2977e5cac9b6a3ebfcb9b2b0b1
Dhanushanth <dhanushanthp@gmail.com>
2024-12-13 21:35:09 +1100

    F

--- FRX_DAILY_PREV_CDL_DIRECTION.bat
+++ FRX_DAILY_PREV_CDL_DIRECTION.bat
@@ -12,7 +12,7 @@ set trades_per_day=100
 REM Risk Management
 set entry_with_st_tgt=no
 set account_risk=1.0
-set account_target_ratio=3.0
+set account_target_ratio=2.5
 set each_position_risk=0.02
 set target_ratio=5.0
 set enable_dynamic_direction=yes
