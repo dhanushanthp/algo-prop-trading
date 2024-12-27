6019c1a78b5665253235aa31888c3c7331032315
Dhanushanth <dhanushanthp@gmail.com>
2024-12-27 11:14:46 +1100

    F

--- modules/common/logme.py
+++ modules/common/logme.py
@@ -2,7 +2,7 @@ import logging
 from modules import config
 
 # Configure logging
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", filename=f'logs_{config.local_ip}.log', level=logging.DEBUG)
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", filename=f'logs/{config.local_ip}.log', level=logging.DEBUG)
 
 def log_it(system:str) -> logging.Logger:
     return logging.getLogger(system)
