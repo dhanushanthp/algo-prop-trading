3ba6b084d7aff1a8e65c524d8a2e938e748e1820
Dhanushanth <dhanushanthp@gmail.com>
2024-12-28 00:13:45 +1100

    Initialize dynamic exit risk-reward ratio and streamline market close position handling in SmartTrader

--- smart_trader.py
+++ smart_trader.py
@@ -328,6 +328,7 @@ class SmartTrader():
                     self.risk_manager = RiskManager(account_risk=self.account_risk,  position_risk=self.each_position_risk,  stop_ratio=self.stop_ratio, 
                                                 target_ratio=self.target_ratio, enable_dynamic_direction=self.enable_dynamic_direction, strategy=self.strategy,
                                                 stop_expected_move=self.stop_expected_move, account_target_ratio=self.account_target_ratio)
+                    self.dynamic_exit_rr = 0.0
                 
 
 
@@ -365,19 +366,15 @@ class SmartTrader():
 
 
             if self.is_market_close:
-                # Don't close the trades on market close if it's more than 4 hour time frame
-                if self.trading_timeframe < 240:
-                    self.orders.close_all_positions()
+                # Close all positions
+                self.orders.close_all_positions()
                 
                 # Update the result in Slack
                 if self.notify_pnl and not self.is_initial_run:
                     self.risk_manager.alert.send_msg(f"{util.get_account_name()} - {config.local_ip} : {self.risk_manager.strategy}-{'|'.join(self.systems)}: ($ {round(self.PnL, 2)})  {round(self.rr, 2)}, ${round(self.equity)}")
                     
                     # Write the pnl to a file
-                    files_util.update_pnl(file_name=util.get_server_ip(), system='|'.join(self.systems), strategy=self.risk_manager.strategy, pnl=self.PnL, rr=self.rr, each_pos_percentage=self.risk_manager.position_risk_percentage)
                     self.trade_tracker.daily_pnl_track(pnl=self.PnL, rr=self.rr, system='|'.join(self.systems), strategy=self.risk_manager.strategy, account_risk_percentage=self.risk_manager.account_risk_percentage, each_position_risk_percentage=self.risk_manager.position_risk_percentage)
-                    # Update the strategy
-                    # self.strategy:str = files_util.get_strategy()
                 
                 # Reset account size for next day
                 self.risk_manager = RiskManager(account_risk=self.account_risk,  position_risk=self.each_position_risk,  stop_ratio=self.stop_ratio, 
