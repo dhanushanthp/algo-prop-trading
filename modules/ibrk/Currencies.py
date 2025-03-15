08389f8a24748bb1496f613cad1ddfbb47adda64
Dhanushanth <dhanushanthp@gmail.com>
2025-03-15 18:12:14 +1100

    Remove unused modules and batch files from the ibrk package

--- modules/ibrk/Currencies.py
+++
@@ -1,9 +0,0 @@
-currencies = ['AUDJPY', 'AUDNZD', 'AUDUSD', 'AUDCHF', "AUDCAD",
-                    'CHFJPY', "CADCHF", "CADJPY",
-                    'EURJPY', 'EURNZD', 'EURUSD', 'EURCAD', "EURAUD", "EURCHF",
-                    'GBPUSD', 'GBPJPY', "GBPAUD", "GBPCHF",
-                    'NZDJPY', "NZDCAD", "NZDUSD",
-                    'USDCAD', 'USDJPY', 'USDCHF', "USDHKD",
-                    'XAUUSD']
-
-jpy_pairs = ['AUDJPY', 'CHFJPY', 'EURJPY', 'GBPJPY' , 'NZDJPY', 'USDJPY', "CADJPY"]
